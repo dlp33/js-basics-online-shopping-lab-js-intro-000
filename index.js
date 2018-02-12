5f3ffce7e45a092a3dc0c4833257cb238ae68975
Darren Paddock <darrenleepaddock@gmail.com>
2018-02-12 17:47:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -82,7 +82,8 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if(arguments.length) {
-    
+    console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`);
+    cart = [];
   } else {
     console.log(`Sorry, we don't have a credit card on file for you.`);
   }
