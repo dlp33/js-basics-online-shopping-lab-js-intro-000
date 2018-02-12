719790f6412777f9b1002392d18b080894033cbc
Darren Paddock <darrenleepaddock@gmail.com>
2018-02-12 16:38:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ var itemArray = [];
   if (cart.length === 3) {
     var item1 = itemArray[0];
     var item2 = itemArray[1];
-    var item3 = itemArray[1];
+    var item3 = itemArray[2];
     console.log(`In your cart, you have ${item1}, ${item2}, and ${item3}.`);
   }
 }
