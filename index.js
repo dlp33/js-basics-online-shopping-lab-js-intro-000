941969752683a76a4b8b9fb4bf545f6427df2ee6
Darren Paddock <darrenleepaddock@gmail.com>
2018-02-12 15:34:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function addToCart(item) {
 
 function viewCart() {
   if (!cart.length) {
-    return "Your shopping cart is empty."
+    return `Your shopping cart is empty.`;
   }
 
   for(var i = 0; i < cart.length; i++) {
