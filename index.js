ed811dd85f404cfe437dd7b6e986271272cd563c
Darren Paddock <darrenleepaddock@gmail.com>
2018-02-12 17:44:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -81,5 +81,9 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if(arguments.length) {
+    
+  } else {
+    console.log(`Sorry, we don't have a credit card on file for you.`);
+  }
 }
